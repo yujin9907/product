0a91641e14e6b2befa9a88efceca516a0f3e0545
yujin9907 <heoyujin07@gmail.com>
2022-09-29 22:24:46 +0900

    import db지정 해도 안 됨

--- firstapp/src/main/java/site/metacoding/firstapp/domain/product/Product.java
+++
@@ -1,5 +0,0 @@
-package site.metacoding.firstapp.domain.product;
-
-public class Product {
-
-}
--- src/test/java/site/metacoding/firstapp/domain/product/ProductDaoTest.java
+++ src/test/java/site/metacoding/firstapp/domain/product/ProductDaoTest.java
@@ -3,11 +3,17 @@ package site.metacoding.firstapp.domain.product;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+
+import site.metacoding.firstapp.config.MyBatisConfig;
 
 //@SpringBootTest 메모리에 모든 걸 다 띄우는 어노테이션
-
+@Import(MyBatisConfig.class) // MyBatisTest가 MyBatisConfig를 못읽음
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // 실DB사용
+@MybatisTest
 public class ProductDaoTest {
 
     // 테스트는 빈 생성자만 new 하기 때문에 어노테이션으로 주입
